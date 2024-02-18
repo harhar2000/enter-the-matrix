3b85f65be41f846b365b3a6c102d0a0666aff1c7
Harry <hmsh2000@gmail.com>
2024-02-18 13:13:24 +0000

    name

--- lib/main.py
+++ lib/main.py
@@ -50,7 +50,7 @@ def link_welcome(name, custom_delay=0.02):
     print_slowly(wrapped_text, delay=custom_delay)
 
 def name_function(name):
-    name
+    name  ### Trying to establish name here to pass to future files
 
 if __name__ == "__main__": 
     enter_the_matrix()
