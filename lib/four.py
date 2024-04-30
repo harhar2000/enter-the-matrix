533731724d20afa65db5b74f92617018258069b5
Harry <hmsh2000@gmail.com>
2024-04-30 22:57:22 +0100

    four

--- lib/four.py
+++ lib/four.py
@@ -4,7 +4,7 @@ def first_function():
     paragraphs = ["\n Let's try defining the function unlock_door(code)"]
 
     wrap_and_print_text(paragraphs)
-
+ 
     def matches_unlock_door_definition(input):
         pattern = r"def unlock_door\(code\):\s*return code == '(\d+)'"
         return re.match(input, pattern)
