dc99017fe4dd204ea6e40a40aa048144e0e75fa3
Harry <hmsh2000@gmail.com>
2024-06-06 09:17:03 +0100

    comments

--- lib/editor.py
+++ lib/editor.py
@@ -7,13 +7,19 @@ import subprocess
 import os
 import tempfile
 
+# Run Visual Studio Code and wait for it to close
 subprocess.run(["C:\\Users\\hmsh2\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code.cmd", "--wait"])
 
+# Check if the 'EDITOR' environment variable is set
 editor = os.environ['EDITOR']
 
+# Create temp file that doesn't delete itself after closing
 tmp = tempfile.NamedTemporaryFile(delete=False)
 tmp.write(b"# this comment works")
 tmp.close()
+
+# Run the editor (specified in the 'EDITOR' environment variable) to open the temporary file
 subprocess.run(editor.split() + [tmp.name], shell=True) 
+# Print the name of the temporary file
 print(tmp.name)
 
